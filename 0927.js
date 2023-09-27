3804baa459bdd2e802a4ee53bb52f80714291590
sooyeon0419 <133744343+sooyeon0419@users.noreply.github.com>
2023-09-27 16:34:56 +0900

    2023_0927
    연산자
    산술, 관계, 논리연산 배움

---
+++ 0927.js
@@ -0,0 +1,26 @@
+//1.string concatenation
+console.log('my'+'cat');
+console.log('1'+2);
+console.log("ellie's\n\tbook");
+
+//2.Numeric operators
+console.log(1+1);//add
+console.log(1-1);//subtract
+console.log(1/1);//divide
+console.log(1*1);//multiply
+console.log(5%2);//remainder
+console.log(2**3);//exponentiation
+
+//5.Comparison operatiors
+console.log(10 < 6);//less than
+console.log(10 <= 6);//less than or equal
+console.log(10 > 6);//greater than
+console.log(10 >= 6);//greater than or equal
+
+//6.Logical operators:||(or), && (and), ! (not)
+const value1 = true;
+const value2 = 4 < 2;
+
+console.log('or:${value1 || value2 || check()}');
+
+console.log('and: ${value1 && value2 && check()}');
